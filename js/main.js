72670f13bb3964519845eb8168a25faec5b50ae3
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-16 15:25:08 -0700

    add file structure

---
+++ js/main.js
@@ -0,0 +1,8 @@
+alert('Remember, this site is under construction.  Thank you for understanding.');
+
+$(document).ready(function(){
+	console.log('loaded');
+	
+	$('.fade').css({ display : 'none' }).fadeIn( 1600 );
+
+});
